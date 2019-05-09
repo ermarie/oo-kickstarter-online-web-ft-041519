0e2cd6436a1a8931c1d5fab64269a4dd94fddd26
Erin Stalzer <erinstalzer@gmail.com>
2019-05-09 15:34:50 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,15 @@
 class Project
 
+  @@backed_projects = []
+
   attr_reader :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
   end
+
+  def bacl_project(Project)
+    @@backed_projects << Project
 
 end
